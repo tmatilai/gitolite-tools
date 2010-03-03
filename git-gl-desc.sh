2a8fa0a97c2523723d9a94785c2406d0b04131dc
Teemu Matilainen <teemu.matilainen@iki.fi>
2010-03-03 23:58:23 +0200

    gl-desc: Rewrite option handling
    NOTE: UI change
    
    gl-desc now has four actions:
      --get  (default)
      --set
      --edt
      --delete
    
    Call "git gl-desc -h" for more help.
    
    Signed-off-by: Teemu Matilainen <teemu.matilainen@iki.fi>

--- git-gl-desc.sh
+++ git-gl-desc.sh
@@ -4,61 +4,96 @@
 #
 # Display or edit description of gitolite wildcard repositories.
 
-NONGIT_OK=Yes
-OPTIONS_SPEC=
-USAGE='(get | set | edit | remove) [<repository>]'
-LONG_USAGE='git gl-desc get [<repository>]
-        display description
-
-git gl-desc set [<repository>]
-        set description from stdin
-
-git gl-desc edir [<repository>]
-        edit description
-
-git gl-desc remove [<repository>]
-        remove description
-
-<repository>   Git URL or remote name'
-
-. git-sh-setup
-
 GL_PATH_NEEDED=Yes
 . git-gl-helpers
 
-getdesc() {
-	resolve_remote "$1"
+NONGIT_OK=Yes
+OPTIONS_SPEC="\
+git gl-desc [--get] [<repository>]
+git gl-desc --set[=<description> | --file=<file>] [<repository>]
+git gl-desc --edit [<repository>]
+git gl-desc --delete [<repository>]
+--
+q,quiet           be quiet
+
+  Actions
+get!              display description (default)
+set?!             set description (from stdin if not specified)
+edit!             edit description
+delete!           remove description
+
+  Options
+F,file=!          set the description from the specified file
+
+    <repository>       Git URL or remote name
+"
+
+. git-sh-setup
+
+action= desc=
+while test $# != 0; do
+	case "$1" in
+	-q|--quiet)
+		GIT_QUIET=1
+		;;
+	--no-quiet)
+		GIT_QUIET=
+		;;
+	--get|--edit|--delete)
+		test -z "$action" || usage
+		action=${1#--}
+		;;
+	--set)
+		test -z "$action" -o "$action" = "set" || usage
+		action="set"
+		case "$2" in
+			-*) ;;
+			*) desc="$2"; shift ;;
+		esac
+		;;
+	-F|--file)
+		test -z "$action" -o "$action" = "set" || usage
+		action="set"
+		desc=$(cat "$2") || exit $?
+		shift
+		;;
+	--)
+		shift
+		break
+		;;
+	*)
+		usage
+		;;
+	esac
+	shift
+done
+test "$#" -le 1 || usage
+test -n "$action" || action="get"
+
+resolve_remote "$1"
+
+case "$action" in
+get)
 	gl_ssh_command getdesc "$GL_PATH"
-}
-
-setdesc() {
-	resolve_remote "$1"
-	gl_ssh_command setdesc "$GL_PATH"
-}
-
-editdesc() {
-	resolve_remote "$1"
+	;;
+set)
+	if test -z "$desc"; then
+		desc=$(cat) || exit $?
+	fi
+	test -n "$GIT_QUIET" && exec >/dev/null
+	printf '%s\n' "$desc" | gl_ssh_command setdesc "$GL_PATH"
+	;;
+edit)
 	set -e
-	desc=$(tempfile -s .gl-desc)
-	trap "rm -f '$desc'" 0 1 2 3 15
-	getdesc > "$desc"
-	git_editor "$desc"
-	test -s "$desc" && setdesc < "$desc"
-}
-
-removedesc() {
-	resolve_remote "$1"
+	desc_file=$(tempfile -s .gl-desc)
+	trap "rm -f '$desc_file'" 0 1 2 3 15
+	gl_ssh_command getdesc "$GL_PATH" > "$desc_file"
+	git_editor "$desc_file"
+	test -n "$GIT_QUIET" && exec >/dev/null
+	test -s "$desc_file" && gl_ssh_command setdesc "$GL_PATH" < "$desc_file"
+	;;
+delete)
+	test -n "$GIT_QUIET" && exec >/dev/null
 	gl_ssh_command setdesc "$GL_PATH" < /dev/null
-}
-
-test "$#" -eq 0 && usage
-cmd="$1"
-shift
-case "$cmd" in
-	help)   git gl-desc -h ;;
-	get)    getdesc  "$@" ;;
-	set)    setdesc  "$@" ;;
-	edit)   editdesc "$@" ;;
-	remove) removedesc "$@" ;;
-	*)      usage ;;
+	;;
 esac
